076c0bccce5ca8898d7b64410099bf83a39a94e8
Federico Repond <federico.repond@leapsight.com>
2017-06-26 08:57:49 -0300

    Refactoring and code cleanup

--- src/wamp_service_worker.erl
+++ src/wamp_service_worker.erl
@@ -2,8 +2,8 @@
 -behaviour(gen_server).
 
 -record(state, {
-        pool_type
-        }).
+          pool_type
+         }).
 
 
 %% API
@@ -33,63 +33,19 @@ init(_Opts) ->
     {ok, #state{pool_type = permanent}}.
 
 
-handle_call({{invocation, RequestId, RegistrationId, _Details, Args, ArgumentsKw}, #{con := Con, callbacks := Callbacks}}, _From, State) ->
-    try
-        #{RegistrationId := #{handler := {Mod, Fun} = Handler}} = Callbacks,
-        lager:info("handle_cast invocation ~p ~p.", [RegistrationId, Handler]),
-        Res = apply(Mod, Fun, Args ++ [ArgumentsKw]),
-        handle_result(Con, RequestId, Res),
-        {reply, Res, State}
-    catch
-        %% @TODO review error handling and URIs
-        Error:Reason ->
-            lager:error("Error: ~p:~p ~n ~p", [Error, Reason, erlang:get_stacktrace()]),
-            awre:error(Con, RequestId, Error, Reason, <<"wamp.error.invalid_argument">>),
-            {noreply, State}
-    end;
-handle_call({{event, SubscriptionId, _PublicationId, _Details, Args, ArgumentsKw}, #{callbacks := Callbacks}}, _From, State) ->
-    try
-        #{SubscriptionId := #{handler := {Mod, Fun} = Handler}} = Callbacks,
-        lager:info("handle_cast event ~p ~p.", [SubscriptionId, Handler]),
-        apply(Mod, Fun, Args ++ [ArgumentsKw]),
-        {noreply, State}
-    catch
-        %% @TODO review error handling and URIs
-        Error:Reason ->
-            lager:error("Error: ~p:~p ~n ~p", [Error, Reason, erlang:get_stacktrace()]),
-            {noreply, State}
-    end;
+handle_call({{invocation, _, _, _, _, _}, _} = Invocation, From, State) ->
+    handle_invocation(Invocation, From, State);
+handle_call({{event, _, _, _, _, _}, _} = Event, From, State) ->
+    handle_event(Event, From, State);
 handle_call(Event, _From, State) ->
     lager:error("Unsupported call ~p", [Event]),
     {noreply, State}.
 
 
-handle_cast({{invocation, RequestId, RegistrationId, _Details, Args, ArgumentsKw}, #{con := Con, callbacks := Callbacks}}, State) ->
-    try
-        #{RegistrationId := #{handler := {Mod, Fun} = Handler}} = Callbacks,
-        lager:info("handle_cast invocation ~p ~p.", [RegistrationId, Handler]),
-        Res = apply(Mod, Fun, Args ++ [ArgumentsKw]),
-        handle_result(Con, RequestId, Res),
-        {noreply, State}
-    catch
-        %% @TODO review error handling and URIs
-        Error:Reason ->
-            lager:error("Error: ~p:~p ~n ~p", [Error, Reason, erlang:get_stacktrace()]),
-            awre:error(Con, RequestId, Error, Reason, <<"wamp.error.invalid_argument">>),
-            {noreply, State}
-    end;
-handle_cast({{event, SubscriptionId, _PublicationId, _Details, Args, ArgumentsKw}, #{callbacks := Callbacks}}, State) ->
-    try
-        #{SubscriptionId := #{handler := {Mod, Fun} = Handler}} = Callbacks,
-        lager:info("handle_cast event ~p ~p.", [SubscriptionId, Handler]),
-        apply(Mod, Fun, Args ++ [ArgumentsKw]),
-        {noreply, State}
-    catch
-        %% @TODO review error handling and URIs
-        Error:Reason ->
-            lager:error("Error: ~p:~p ~n ~p", [Error, Reason, erlang:get_stacktrace()]),
-            {noreply, State}
-    end;
+handle_cast({{invocation, _, _, _, _, _}, _} = Invocation, State) ->
+    handle_invocation(Invocation, undefined, State);
+handle_cast({{event, _, _, _, _, _}, _} = Event, State) ->
+    handle_event(Event, undefined, State);
 handle_cast(Event, State) ->
     lager:error("Unsupported cast ~p", [Event]),
     {noreply, State}.
@@ -119,12 +75,55 @@ code_change(_OldVsn, State, _Extra) ->
 %% PRIVATE
 %% =============================================================================
 
-handle_result(Con, RequestId, Res) ->
+handle_invocation({{invocation, RequestId, RegistrationId, Details, Args, ArgsKw},
+                   #{con := Con, callbacks := Callbacks}}, _From, State) ->
+    try
+        #{RegistrationId := #{handler := {Mod, Fun} = Handler, scopes := Scopes}} = Callbacks,
+        lager:info("handle_cast invocation ~p ~p ~p.", [RegistrationId, Handler, Scopes]),
+        handle_security(ArgsKw, Scopes),
+        Res = apply(Mod, Fun, Args ++ [ArgsKw]),
+        handle_result(Con, RequestId, Details, Res, ArgsKw),
+        {noreply, State}
+    catch
+        %% @TODO review error handling and URIs
+        unauthorized ->
+            awre:error(Con, RequestId, <<"unauthorized">>,  <<"Unauthorized user">>, <<"com.magenta.error.unauthorized">>);
+        not_found ->
+            awre:error(Con, RequestId, <<"not_found">>, <<"Resource not found">>, <<"com.magenta.error.not_found">>);
+        Error:Reason ->
+            lager:error("Error: ~p:~p ~n ~p", [Error, Reason, erlang:get_stacktrace()]),
+            awre:error(Con, RequestId, Error, Reason, <<"wamp.error.invalid_argument">>),
+            {noreply, State}
+    end.
+
+
+handle_event({{event, SubscriptionId, _PublicationId, _Details, Args, ArgsKw},
+              #{callbacks := Callbacks}}, _From, State) ->
+    try
+        #{SubscriptionId := #{handler := {Mod, Fun} = Handler}} = Callbacks,
+        lager:info("handle_cast event ~p ~p.", [SubscriptionId, Handler]),
+        apply(Mod, Fun, Args ++ [ArgsKw]),
+        {noreply, State}
+    catch
+        %% @TODO review error handling and URIs
+        Error:Reason ->
+            lager:error("Error: ~p:~p ~n ~p", [Error, Reason, erlang:get_stacktrace()]),
+            {noreply, State}
+    end.
+
+
+handle_result(Con, RequestId, Details, Res, ArgsKw) ->
     case Res of
         undefined ->
-            ok = awre:yield(Con, RequestId, [], []);
+            ok = awre:yield(Con, RequestId, Details, [], ArgsKw);
         notfound ->
-            awre:error(Con, RequestId, <<"not_found">>, <<"Resource not found">>, <<"com.magenta.error.not_found">>);
+            throw(not_found);
         _ ->
-            ok = awre:yield(Con, RequestId, [], [Res])
+            ok = awre:yield(Con, RequestId, Details, [Res], ArgsKw)
     end.
+
+
+handle_security(_ArgsKw, []) ->
+    ok;
+handle_security(_ArgsKw, _Scopes) ->
+    throw({unauthorized}).
