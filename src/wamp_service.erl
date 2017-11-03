ee84a8ce800084a479f3572fa1b22c3f99ef0986
Federico Repond <frepond@gmail.com>
2017-11-03 12:44:57 -0300

    Fixed return types

--- src/wamp_service.erl
+++ src/wamp_service.erl
@@ -7,28 +7,29 @@
 -export([call/3, call/4, maybe_error/1, publish/3]).
 
 
--spec call(Uri :: binary(), Args :: term(), Opts :: map()) -> term().
+-spec call(Uri :: binary(), Args :: term(), Opts :: map()) -> {ok, any()} | {error, binary(), map()} | no_return().
 call(Uri, Args, Opts) ->
     call(Uri, Args, Opts, 10000).
 
--spec call(Uri :: binary(), Args :: term(), Opts :: map(), Timeout :: pos_integer()) -> term().
+-spec call(Uri :: binary(), Args :: term(), Opts :: map(), Timeout :: pos_integer()) ->
+    {ok, any()} | {error, binary(), map()} | no_return().
 call(Uri, Args, Opts, Timeout) ->
     process_flag(trap_exit, true),
     WampRes = poolboy:transaction(wamp_call_sessions,
                                   fun(Worker) ->
                                           gen_server:call(Worker, {call, Uri, Args, Opts, Timeout}, infinity)
                                   end, infinity),
-    lager:debug("call uri=~p result=~p", [Uri, WampRes]),
+    ok = lager:debug("call uri=~p result=~p", [Uri, WampRes]),
     case WampRes of
         {ok, _, [Res], _} ->
-            Res;
+            {ok, Res};
         {ok, _, [], _} ->
-            ok;
+            {ok, undefined};
         {error, _, Key, _, Map} ->
             {error, Key, Map}
     end.
 
--spec maybe_error(term()) -> term() | no_return().
+-spec maybe_error(term()) -> {ok, any()} | no_return().
 maybe_error(WampRes) ->
     case WampRes of
         Error = {error, _Key, _Map} ->
