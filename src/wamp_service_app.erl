50619446a0a772efa6a96cf95699a466a9c4aead
Federico C. Repond <frepond@gmail.com>
2018-03-15 14:32:30 -0300

    Added defualt ping and log level procedures

--- src/wamp_service_app.erl
+++ src/wamp_service_app.erl
@@ -15,7 +15,9 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    wamp_service_sup:start_link().
+    Res = wamp_service_sup:start_link(),
+    register_services(),
+    Res.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
@@ -24,3 +26,13 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+register_services() ->
+    {_, Host} = service_and_host(),
+    Ping = <<"com.magenta.", Host/binary, ".ping">>,
+    LogLevel = <<"com.magenta.", Host/binary, ".log_level">>,
+    wamp_service:register(wamp_sessions, procedure, Ping, fun wamp_service_instr:ping/1, [<<"admin">>]),
+    wamp_service:register(wamp_sessions, procedure, LogLevel, fun wamp_service_instr:log_level/2, [<<"admin">>]).
+
+service_and_host() ->
+    [Service, Host] = binary:split(atom_to_binary(node(), utf8), [<<"@">>]),
+    {Service, Host}.
