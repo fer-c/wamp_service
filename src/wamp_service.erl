2ff85fbb1f50b8bd11064c6ef0fcf94c0b24fb79
Federico C. Repond <frepond@gmail.com>
2018-03-15 19:24:54 -0300

    wip refactor dispatcher/service load regulation

--- src/wamp_service.erl
+++ src/wamp_service.erl
@@ -4,7 +4,7 @@
 
 -module(wamp_service).
 
--export([call/3, call/4, maybe_error/1, publish/3, register/4, register/5, unregister/2]).
+-export([call/3, call/4, maybe_error/1, publish/3, register/3, register/4, unregister/1]).
 
 
 -spec call(Uri :: binary(), Args :: term(), Opts :: map()) -> {ok, any()} | {error, binary(), map()} | no_return().
@@ -15,11 +15,11 @@ call(Uri, Args, Opts) ->
                   {ok, any()} | {error, binary(), map()} | no_return().
 call(Uri, Args, Opts, Timeout) when is_list(Args) ->
     process_flag(trap_exit, true),
-    WampRes = poolboy:transaction(wamp_call_sessions,
+    WampRes = poolboy:transaction(service,
                                   fun(Worker) ->
                                           gen_server:call(Worker, {call, Uri, Args, Opts, Timeout}, infinity)
                                   end, infinity),
-    ok = lager:debug("call uri=~p result=~p", [Uri, WampRes]),
+    _ = lager:debug("call uri=~p result=~p", [Uri, WampRes]),
     case WampRes of
         {ok, _, [Res], _} ->
             {ok, Res};
@@ -40,28 +40,28 @@ maybe_error(WampRes) ->
 
 -spec publish(Topic :: binary(), Args :: [any()], Opts :: map()) -> ok | no_return().
 publish(Topic, Args, Opts) when is_list(Args) ->
-    poolboy:transaction(wamp_call_sessions, fun(Worker) ->
-                                                    gen_server:call(Worker, {publish, Topic, Args, Opts})
-                                            end).
+    poolboy:transaction(service, fun(Worker) ->
+                                         gen_server:call(Worker, {publish, Topic, Args, Opts})
+                                 end).
 
--spec register(atom(), procedure | subscription, binary(), {atom(), atom()} | function())
+-spec register(procedure | subscription, binary(), {atom(), atom()} | function())
               -> ok | {error, binary()} | no_return().
-register(Pool, procedure, Uri, Handler) ->
-    register(Pool, procedure, Uri, Handler, []);
-register(Pool, subscription, Uri, Handler) ->
-    poolboy:transaction(Pool, fun(Worker) ->
-                                      gen_server:call(Worker, {register, Uri, {subscription, Handler}})
-                              end).
+register(procedure, Uri, Handler) ->
+    register(procedure, Uri, Handler, []);
+register(subscription, Uri, Handler) ->
+    poolboy:transaction(dispatcher, fun(Worker) ->
+                                            gen_server:call(Worker, {register, Uri, {subscription, Handler}})
+                                    end).
 
--spec register(atom(), procedure | subscription, binary(), {atom(), atom()} | function(), [binary()])
+-spec register(procedure | subscription, binary(), {atom(), atom()} | function(), [binary()])
               -> ok | {error, binary()} | no_return().
-register(Pool, procedure, Uri, Handler, Scopes) ->
-    poolboy:transaction(Pool, fun(Worker) ->
-                                      gen_server:call(Worker, {register, Uri, {procedure, Handler, Scopes}})
-                              end).
+register(procedure, Uri, Handler, Scopes) ->
+    poolboy:transaction(dispatcher, fun(Worker) ->
+                                            gen_server:call(Worker, {register, Uri, {procedure, Handler, Scopes}})
+                                    end).
 
--spec unregister(atom(), binary()) -> ok | {error, binary()} | no_return().
-unregister(Pool, Uri) ->
-    poolboy:transaction(Pool, fun(Worker) ->
-                                      gen_server:call(Worker, {unregister, Uri})
-                              end).
+-spec unregister(binary()) -> ok | {error, binary()} | no_return().
+unregister(Uri) ->
+    poolboy:transaction(dispatcher, fun(Worker) ->
+                                            gen_server:call(Worker, {unregister, Uri})
+                                    end).
