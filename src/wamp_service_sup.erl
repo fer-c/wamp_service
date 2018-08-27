7d4370db7983a9a280acbd0409b736700125a093
Federico C. Repond <federico.repond@leapsight.com>
2018-08-27 17:39:43 -0300

    tweak restarts

--- src/wamp_service_sup.erl
+++ src/wamp_service_sup.erl
@@ -34,11 +34,11 @@ init([]) ->
     {workers, SWorkers} = lists:keyfind(workers, 1, ServiceSpec),
     ArgsD = [callee_dispatcher, [
                 {workers, DWorkers}, {worker, {wamp_service_dispatcher, DispatcherSpec}},
-                {strategy, {one_for_all, 1, 1}}]],
+                {strategy, {one_for_all, 0, 1}}]],
     ArgsS = [caller_service, [
                 {workers, SWorkers}, {worker, {wamp_service_service, ServiceSpec}},
-                {strategy, {one_for_all, 1, 1}}]],
-    {ok, {{one_for_one, 10, 10}, [
+                {strategy, {one_for_all, 0, 1}}]],
+    {ok, {{one_for_one, 5, 10}, [
         {callee_dispatcher, {wpool, start_sup_pool, ArgsD}, permanent, 5000, supervisor, []},
         {caller_service, {wpool, start_sup_pool, ArgsS}, permanent, 5000, supervisor, []}
     ]}}.
