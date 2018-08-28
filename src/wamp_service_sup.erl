0ed061e3cd2fca6f2eac371bc14892632884d92e
Federico C. Repond <federico.repond@leapsight.com>
2018-08-28 09:45:18 -0300

    changed restart strategy

--- src/wamp_service_sup.erl
+++ src/wamp_service_sup.erl
@@ -30,7 +30,7 @@ start_link() ->
 init([]) ->
     {ok, DispatcherSpec} = application:get_env(wamp_service, callee_dispatcher),
     {ok, ServiceSpec} = application:get_env(wamp_service, caller_service),
-    {ok, {{one_for_all, 5, 10}, [
+    {ok, {{one_for_one, 3, 60}, [
         {callee_dispatcher, {wamp_service_dispatcher, start_link, [DispatcherSpec]}, permanent, 5000, worker, []},
         {caller_service, {wamp_service_service, start_link, [ServiceSpec]}, permanent, 5000, worker, []}
     ]}}.
